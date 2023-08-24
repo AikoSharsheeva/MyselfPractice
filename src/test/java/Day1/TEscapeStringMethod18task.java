c026f32aadaac623e01389ced78e80e29f128395
AikoSharsheeva <calipsolee@gmail.com>
2023-08-24 21:44:42 +0600

    T Escape character with String 18 task

---
+++ src/test/java/Day1/TEscapeStringMethod18task.java
@@ -0,0 +1,16 @@
+package Day1;
+
+public class TEscapeStringMethod18task {
+    public static void main(String[] args) {
+        String meetingName1 = "Daily Standup";
+        String startTime1 = "9:15am";
+        String endTime1 = "9:45am";
+        String meetingName2 = "Test Strategy Meeting";
+        String startTime2 = "14:15pm";
+        String endTime2 = "15:15pm";
+        System.out.println(meetingName1 + "\t\t\t\t\t" + "Start time: " + startTime1+"\t\t"+
+                "End time: " + endTime1 +
+                "\n" + meetingName2 + "\t\t\t" + "Start time: " + startTime2 +"\t\t"+
+                "End time: " + endTime2);
+    }
+}
