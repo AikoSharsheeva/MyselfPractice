abfae84d93e1e49e49d6cc5082043c255da8b95c
AikoSharsheeva <calipsolee@gmail.com>
2023-08-24 20:36:59 +0600

    Increment 13 task

---
+++ src/test/java/Day1/Increment13Task.java
@@ -0,0 +1,20 @@
+package Day1;
+
+public class Increment13Task {
+    public static void main(String[] args) {
+        int num=0;
+        num++;
+        System.out.println(num);
+        num++;
+        System.out.println(num);
+        num++;
+        System.out.println(num);
+        num++;
+        System.out.println(num);
+        num++;
+        System.out.println(num);
+        int num2=5;
+        num2+=5;
+        System.out.println(num2);
+    }
+}
