1674fb85ceb6ca7fd6bbd04127886f91f61d7869
AikoSharsheeva <calipsolee@gmail.com>
2023-08-24 19:04:52 +0600

    using for loop and array

---
+++ src/test/java/Day1/ArrayTask6.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class ArrayTask6 {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford"};
+        for
+        (String i : cars) {
+            System.out.println(i);
+        }
+    }
+}
